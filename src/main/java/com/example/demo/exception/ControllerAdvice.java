a503d31d4c409e5f4eb998ceb5f16c857952962e
CarlosVF01 <carlosfornesvela@gmail.com>
2021-11-23 11:13:13 +0100

    a

--- src/main/java/com/example/demo/exception/ControllerAdvice.java
+++ src/main/java/com/example/demo/exception/ControllerAdvice.java
@@ -1,7 +1,6 @@
 package com.example.demo.exception;
 
-import com.example.demo.exception.ElementNotFoundException;
-import com.example.demo.exception.EmptyBodyException;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
